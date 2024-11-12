9e4fa05d958cf5eaf8130e8f927b23526d163bc6
2504pratik <21bcs053@iiitdwd.ac.in>
2024-11-12 16:45:36 +0530

    Docker and Jenkins

--- doctor-service/src/main/java/com/example/doctorservice/controller/DoctorController.java
+++ doctor-service/src/main/java/com/example/doctorservice/controller/DoctorController.java
@@ -5,8 +5,10 @@ import com.example.doctorservice.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+@PreAuthorize("hasRole('ROLE_DOCTOR')")
 @RestController
 @RequestMapping("/api/doctor")
 public class DoctorController {
